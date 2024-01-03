8524ffbbb8d51ea579089258731c9931671f3eb8
Chris-Wilco <christopher.wilcoski@gmail.com>
2024-01-03 13:16:47 -0800

    Add permanent css class change to grid square on hover

--- scripts.js
+++ scripts.js
@@ -10,9 +10,22 @@ function createGrid(){
 
         for(let j = 0; j < 4; j++){
             let newDiv = document.createElement('div');
+            let divText = document.createElement('div');
+
             newDiv.id = `grid-div-${i}-${j}`;
+            newDiv.classList.add('fade-in-grid-square');
+            divText.textContent = `${newDiv.id}`;
+
+            newDiv.addEventListener('mouseover', function (e) {
+                this.classList.toggle('fade-in-grid-square-highlighted');
+            });
+
+            
+
+            newDiv.appendChild(divText);
             rowDiv.appendChild(newDiv);
         }
         gridContainer.appendChild(rowDiv);
     }
 }
+
