5d11e51a4a4a90af5a5ca0bae84ef0213e22c736
Chris-Wilco <christopher.wilcoski@gmail.com>
2024-01-05 15:07:33 -0800

    add button functionality for resize capability

--- scripts.js
+++ scripts.js
@@ -2,28 +2,34 @@
 const gridContainer = document.getElementById('grid-container');
 const resizeButton = document.getElementById('resize-button');
 
-createGrid(8);
 
-resizeButton.addEventListener('click', function(e){
+resizeButton.addEventListener('click', function (e) {
     promptForGridSize();
 });
 
 function promptForGridSize(){
     let x = prompt("What size would you like the grid to be?", "Please enter an integer between 1 and 100.");
+    createGrid(x);
 
-    if(Number.isInteger(x) && x > 0 && x <= 100){
-        createGrid();
+    if(!isNaN(x) && x >= 0 && x <= 100){
+        createGrid(x);
     } else {
         promptForGridSize();
     }
+    
 }
 
+createGrid(8);
 
 
 function createGrid(gridDimension){
+    //gridContainer.innerHTML('');
+    removeAllChildNodes(gridContainer);
+
     for(let i = 0; i < gridDimension; i++){
         let rowDiv = document.createElement('div');
         rowDiv.id = `row-div-${i}`;
+        rowDiv.classList.add('grid-row');
 
         for(let j = 0; j < gridDimension; j++){
             let newDiv = document.createElement('div');
@@ -31,6 +37,7 @@ function createGrid(gridDimension){
 
             newDiv.id = `${i}-${j}`;
             newDiv.classList.add('fade-in-grid-square');
+            newDiv.classList.add('grid-square');
             divText.textContent = `${newDiv.id}`;
 
             newDiv.addEventListener('mouseover', function (e) {
@@ -44,3 +51,8 @@ function createGrid(gridDimension){
     }
 }
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
